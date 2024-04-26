ad6f566e933b2c421df2680e14c62d8d056a7782
Kinshuk-01 <ckinshuk01@gmail.com>
2024-04-26 14:28:09 +0530

    add age

--- Hello.cpp
+++ Hello.cpp
@@ -6,6 +6,10 @@ int main(){
     string name;
     std::cout<<"Enter your name: ";
     std::cin>>name;
+
+    int age;
+    std::cout<<"Enter your age: ";
+    std::cin>>age;
     
     return 0;
 }
